02688f3aaf1a717858df9ee1a5ab0ee0275ca2a8
bharath-pasham <bharath.pasham@gmail.com>
2017-07-07 21:52:08 +0200

    Adding project zippy

---
+++ zippy/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector : 'my-app',
+    template: `
+        <zippy title="Who can see my stuff?">
+            Content of the stuff
+            </zippy>
+                `
+})
+
+export class AppComponent {
+
+}
