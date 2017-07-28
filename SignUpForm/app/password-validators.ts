7fc65c7e168aa60fd1273a1c15fa82f3ab09f8c7
bharath pasham <bharath.pasham@gmail.com>
2017-07-28 17:38:21 +0200

    modifications to get confirm password work

--- SignUpForm/app/password-validators.ts
+++ SignUpForm/app/password-validators.ts
@@ -4,7 +4,7 @@ export class PasswordValidators {
     static minLength(control: FormControl) {
      
        
-                console.log("checking length"+control.value.length);
+                // console.log("checking length"+control.value.length);
                 if (control.value.length < 4) {
                     
                     return { minLength: true };
@@ -14,17 +14,22 @@ export class PasswordValidators {
            
       
     }
+
+    /*
+    *   The returned value should be accessed in a different way
+    */
     static matchingPassword(group : FormGroup) {
-        
-     
         let pwdControl = group.controls.password;
+        let confirmPwdControl = group.controls.confirm_password;
+        console.log(pwdControl.value+" "+confirmPwdControl.value);
         //let confirmPwdControl = group.controls.confirm_password;
 
 
-        if (pwdControl) {
+        if (pwdControl.value != confirmPwdControl.value) {
+            console.log("Dont macth");
             return {
                 notMatching: true
-            }
+            };
         }
         return null;
 
